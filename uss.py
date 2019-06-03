0bfa552ab2fae5dce51e62124c2d0d113c84632e
Janko Smejkal <48658169+JankoSmejkal@users.noreply.github.com>
2019-06-03 14:45:51 +0100

    Update uss.py

--- uss.py
+++ uss.py
@@ -6,13 +6,13 @@ import time
 # Set GPIO pins
 GPIO.setmode(GPIO.BCM) # BOARD does not work with gpiozero library (I did not put much effort into making it work with BOARD)
 sensor = DistanceSensor(echo = 14, trigger = 15) # Sensor's echo and trigger pin
-GPIO.setup(11, GPIO.OUT) # LED pin
+GPIO.setup(25, GPIO.OUT) # LED pin
 
 try:
     while True:
-        GPIO.output(11, True)
+        GPIO.output(25, True)
         time.sleep(sensor.distance) # Use distance value from sensor to control LED blinking frequency
-        GPIO.output(11,False)
+        GPIO.output(25,False)
         time.sleep(sensor.distance) #  Use distance value from sensor to control LED blinking frequency
 
 except KeyboardInterrupt:
